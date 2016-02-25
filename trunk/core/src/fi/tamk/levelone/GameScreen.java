fae9567b09ea0996fd447519b051e99ddb12baff
c5vhaapa <c5vhaapa@1db7d3ad-7c5b-4c76-a90e-26f74cc0e9a5>
2016-02-25 12:18:17 +0000

    kaikkee

--- trunk/core/src/fi/tamk/levelone/GameScreen.java
+++ trunk/core/src/fi/tamk/levelone/GameScreen.java
@@ -6,17 +6,25 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 
 /**
  * Created by Aprona on 24.2.2016.
  */
 public class GameScreen implements Screen {
+    private float WINDOW_WIDTH = 8f;
+    private float WINDOW_HEIGHT = 4.8f;
+    private float WORLD_WIDTH_METERS = 4096f / 100;
+    private float WORLD_HEIGHT_METERS = 2048 / 100;
 
     private Main game;
     private OrthographicCamera camera;
@@ -50,12 +58,18 @@ public class GameScreen implements Screen {
     public GameScreen(Main g) {
         game = g;
         batch = this.game.getBatch();
+        tiledMap = (new TmxMapLoader()).load("background.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 1/100f);
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 8f, 4.8F);
-        tiledMap = (new TmxMapLoader()).load("background.tmx");
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 1/100);
-
         safetySanta = new SafetySanta(this);
+
+        safetySanta.setX(0.64f);
+        safetySanta.setY(0.96f);
+
+        MapLayer stairwayLayer = tiledMap.getLayers().get("stairway_objects");
+        MapObjects stairwayObjects = stairwayLayer.getObjects();
+
     }
 
     @Override
@@ -67,8 +81,11 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         clearScreen();
         batch.setProjectionMatrix(camera.combined);
+        camera.update();
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
+
+        moveCamera();
 
         if (Gdx.input.isTouched()) {
             float realX = Gdx.input.getX();
@@ -81,10 +98,11 @@ public class GameScreen implements Screen {
             // Gdx.app.log("touchPosX", String.valueOf(touchPos.x));
             // Gdx.app.log("touchPosY", String.valueOf(touchPos.y));
         }
+        safetySanta.moveSantaKeyboard();
 
         batch.begin();
-        drawButtons();
         safetySanta.santaDraw(batch);
+        drawButtons();
         batch.end();
     }
 
@@ -107,6 +125,39 @@ public class GameScreen implements Screen {
                 buttonRightRect.y,
                 buttonRightImg.getWidth() / 100f,
                 buttonRightImg.getHeight() / 100f);
+    }
+
+    private void checkCollisions () {
+        MapLayer collisionObjectLayer = (MapLayer)tiledMap.getLayers().get("wall_objects");
+        MapObjects mapObjects = collisionObjectLayer.getObjects();
+        Array<RectangleMapObject> rectangleObjects = mapObjects.getByType(RectangleMapObject.class);
+        for (RectangleMapObject rectangleObject : rectangleObjects) {
+            Rectangle rectangle = rectangleObject.getRectangle();
+            /*if () {
+
+            }*/
+        }
+    }
+
+    private void moveCamera () {
+        camera.position.x = safetySanta.getX() + (safetySanta.getRectangleWidth() / 2);
+        camera.position.y = safetySanta.getY() + (safetySanta.getRectangleHeight() / 2);
+
+        if (camera.position.x > (WORLD_WIDTH_METERS - WINDOW_WIDTH / 2)) {
+            camera.position.x = (WORLD_WIDTH_METERS - WINDOW_WIDTH / 2);
+        }
+
+        if (camera.position.x < WINDOW_WIDTH / 2) {
+            camera.position.x = WINDOW_WIDTH / 2;
+        }
+
+        if (camera.position.y > (WORLD_HEIGHT_METERS - WINDOW_HEIGHT / 2)) {
+            camera.position.y = (WORLD_HEIGHT_METERS - WINDOW_HEIGHT / 2);
+        }
+
+        if (camera.position.y < WINDOW_HEIGHT / 2) {
+            camera.position.y = WINDOW_HEIGHT / 2;
+        }
     }
 
     @Override
